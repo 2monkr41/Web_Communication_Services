d295a6031cbfb196fdec370a4c397d41fcc1431f
2monkr41 <2monkr41@solent.ac.uk>
2017-03-20 17:05:09 +0000

    Search by Oasis

---
+++ app/src/main/java/com/example/a2monkr41/web_communication_services/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.a2monkr41.web_communication_services;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import android.app.AlertDialog;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    class MyTask extends AsyncTask<Void,Void,String>
+    {
+        public String doInBackground(Void... unused)
+        {
+            HttpURLConnection conn = null;
+            try
+            {
+                URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist=Oasis");
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream in = conn.getInputStream();
+                if(conn.getResponseCode() == 200)
+                {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String result = "", line;
+                    while((line = br.readLine()) !=null)
+                        result += line;
+                    return result;
+                }
+                else
+                    return "HTTP ERROR: " + conn.getResponseCode();
+
+
+            }
+            catch(IOException e)
+            {
+                return e.toString();
+            }
+            finally
+            {
+                if(conn!=null)
+                    conn.disconnect();
+            }
+        }
+
+        public void onPostExecute(String result)
+        {
+            TextView tv1 = (TextView)findViewById(R.id.tv1);
+            tv1.setText(result);
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button go = (Button)findViewById(R.id.btn1);
+        go.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        MyTask t = new MyTask();
+        t.execute();
+    }
+}
